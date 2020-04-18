2a9485037fd97268ba895bf570302d57a44cf827
Sarah Kennelly <sek40@uclive.ac.nz>
2020-04-18 22:34:23 +1200

    Adding views to projects

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -49,6 +49,7 @@ struct ContentView: View {
                 }
                 Section {
                     Text("$\(totalPerPerson, specifier: "%.2f")")
+                        .foregroundColor(tipPercentage==4 ? .red : .black)
                 }
             }
         .navigationBarTitle("WeSplit")
