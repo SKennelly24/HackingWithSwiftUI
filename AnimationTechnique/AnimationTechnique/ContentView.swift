b7f9fa05671518be055a14ba5e7250aa3fc3c97b
Sarah Kennelly <sek40@uclive.ac.nz>
2020-04-27 21:25:25 +1200

    Finished technique project

--- AnimationTechnique/AnimationTechnique/ContentView.swift
+++ AnimationTechnique/AnimationTechnique/ContentView.swift
@@ -9,12 +9,90 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var animationAmount: CGFloat = 1
+    @State private var isShowingRed = false
+    
     var body: some View {
-        
-        
+        VStack {
+            Button("Tap me") {
+                withAnimation {
+                    self.isShowingRed.toggle()
+                }
+            }
+            
+            if isShowingRed {
+                Rectangle()
+                    .fill(Color.red)
+                    .frame(width: 200, height:200)
+                    .transition(.pivot)
+            }
+        }
     }
 }
+
+struct CornerRotateModifier: ViewModifier {
+    let amount : Double
+    let anchor : UnitPoint
+    
+    func body(content: Content) -> some View {
+        content.rotationEffect(.degrees(amount), anchor: anchor).clipped()
+    }
+    
+}
+
+extension AnyTransition {
+    static var pivot: AnyTransition {
+        .modifier(active: CornerRotateModifier(amount: -90, anchor:.topLeading), identity: CornerRotateModifier(amount: 0, anchor:.topLeading))
+    }
+}
+/* Tap and rectangle appears and dissapears
+ @State private var isShowingRed = false
+ 
+ var body: some View {
+     VStack {
+         Button("Tap me") {
+             withAnimation {
+                 self.isShowingRed.toggle()
+             }
+         }
+         
+         if isShowingRed {
+             Rectangle()
+                 .fill(Color.red)
+                 .frame(width: 200, height:200)
+                 .transition(.asymmetric(insertion: .scale, removal: .opacity))
+         }
+     }
+ }*/
+
+/* Hello SwfitUI where all the letters animate differently
+ struct ContentView: View {
+     let letters = Array("Hello SwiftUI")
+     @State private var enabled = false
+     @State private var dragAmount = CGSize.zero
+     
+     var body: some View {
+         HStack(spacing: 0) {
+             ForEach(0..<letters.count) { num in
+                 Text(String(self.letters[num]))
+                     .padding(5)
+                     .font(.title)
+                     .background(self.enabled ? Color.blue : Color.red)
+                     .offset(self.dragAmount)
+                     .animation(Animation.default.delay(Double(num) / 20))
+             }
+         }
+         .gesture(
+             DragGesture()
+                 .onChanged { self.dragAmount = $0.translation}
+                 .onEnded { _ in
+                     self.dragAmount = .zero
+                     self.enabled.toggle()
+                     
+                 }
+         )
+     }
+ }
+ */
 
 /*
  Red button with scaling circle forever code
@@ -59,6 +137,31 @@ struct ContentView: View {
      .clipShape(Circle())
      .scaleEffect(animationAmount)
  }*/
+
+/* Red button flipping
+ Button("Tap Me") {
+    withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
+             self.animationAmount += 360
+         }
+ }
+ .padding(50)
+ .background(enabled ? Color.blue : Color.red)
+ .foregroundColor(.white)
+ .clipShape(Circle())
+ .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
+ */
+
+/*Multiple animations
+ Button("Tap Me") {
+     self.enabled.toggle()
+ }
+ .frame(width: 200, height: 200)
+ .background(enabled ? Color.blue : Color.red)
+ .animation(.default)
+ .foregroundColor(.white)
+ .clipShape(RoundedRectangle(cornerRadius: enabled ? 60 : 0))
+ .animation(.interpolatingSpring(stiffness: 10, damping: 1))
+ */
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
