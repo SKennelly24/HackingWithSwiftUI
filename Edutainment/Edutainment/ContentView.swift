0ffefb83c582c7ea35908138b3f9aafcfd314e70
Sarah Kennelly <sek40@uclive.ac.nz>
2020-05-02 17:18:02 +1200

    Finished game

--- Edutainment/Edutainment/ContentView.swift
+++ Edutainment/Edutainment/ContentView.swift
@@ -12,9 +12,14 @@ struct ContentView: View {
     @State private var multiplier = 5
     let numQuestionsList = [5,10,20,100]
     @State private var numQuestions = 1
-    @State private var isGameActive = true
-    @State private var timeTables = []
-    @State private var questionIndexes = Array<(String, Int)>()
+    @State private var isGameActive = false
+    @State private var allQuestions = Array<Question>()
+    @State private var chosenQuestions = Array<Question>()
+    @State private var currentQuestion = 0
+    @State private var answers = []
+    @State private var answer = ""
+    @State private var score = 0
+    @State private var showingAlert = false
     var body: some View {
         Group {
             if isGameActive {
@@ -47,9 +52,9 @@ struct ContentView: View {
                     }
                 }
             }
-            .navigationBarTitle("Game Settings")
+            .navigationBarTitle("Multiplication Game")
             .navigationBarItems(trailing:
-                Button(action: self.startGame) {
+                Button(action: startGame) {
                     Text("Start Game")
                 }
             )
@@ -60,47 +65,73 @@ struct ContentView: View {
         isGameActive = true
         for i in 1...multiplier {
             for j in 1...multiplier {
-                let table = "\(i) x \(j)"
-                let timesTable = (tableString: table, answer: i*j)
-                self.timeTables.append(timesTable)
+                self.allQuestions.append(Question(multiplierA: i, multiplierB: j, answer: i*j))
             }
         }
-        self.questionIndexes = findRandomQuestions()
+        self.chosenQuestions = self.findRandomQuestions()
     }
     
-    func findRandomQuestions() -> Array<(String, Int)> {
-        var questionTuples = [("", 40)]
+    func findRandomQuestions() -> Array<Question> {
+        var questions = Array<Question>()
         var count = 0
         if self.numQuestionsList[self.numQuestions] == 100 {
-            count = self.timeTables.count
+            count = self.allQuestions.count
         } else {
             count = self.numQuestionsList[self.numQuestions]
         }
         
         for _ in 0...count {
-            let timeTableIndex = Int.random(in: 0...self.timeTables.count)
-            let timeTableTuple = self.timeTables.remove(at: timeTableIndex)
-            questionTuples.append(timeTableTuple as! (String, Int))
+            let timeTableIndex = Int.random(in: 0...(self.allQuestions.count-2))
+            let timeTable = self.allQuestions.remove(at: timeTableIndex)
+            questions.append(timeTable)
         }
         
-        return questionTuples
+        return questions
     }
     
     func gameScreen() -> some View {
-        
         NavigationView {
-            VStack {
-                ForEach(0 ..< self.numQuestionsList[self.numQuestions]) { num in
-                    Text("\(self.questionIndexes[num].0)")
+            Form {
+                VStack {
+                    Text("\(self.chosenQuestions[self.currentQuestion].multiplierA) X \(self.chosenQuestions[self.currentQuestion].multiplierB)")
+                    TextField("Answer", text:$answer)
+                    Button(action: nextQuestion) {
+                        Text("Next")
+                    }
                 }
-                
+            }
+            .alert(isPresented: $showingAlert) {
+                Alert(title: Text("Finished Game"), message: Text("Your score was \(self.score)"), dismissButton: .default(Text("Play again")) {
+                    self.isGameActive = false
+                    self.score = 0
+                })
             }
         }
     }
-    func generateQuestions() {
+    
+    func nextQuestion() {
+        if (Int(answer) ?? 0 == self.chosenQuestions[self.currentQuestion].answer) {
+            self.score += 1
+        }
+        if (self.currentQuestion < (self.chosenQuestions.count-2)) {
+            self.currentQuestion += 1
+        } else {
+            self.showingAlert = true
+            self.currentQuestion = 0
+            self.allQuestions.removeAll()
+            
+        }
         
     }
+    
+    struct Question {
+        var multiplierA : Int
+        var multiplierB : Int
+        var answer : Int
+    }
 }
+
+
 
 
 
